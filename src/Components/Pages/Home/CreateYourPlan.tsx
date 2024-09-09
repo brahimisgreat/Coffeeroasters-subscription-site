ab0a1d1b18932f9119ee6f92ce63a76fe30fa916
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-09-08 22:46:52 -0400

    home page our collection

--- src/Components/Pages/Home/CreateYourPlan.tsx
+++ src/Components/Pages/Home/CreateYourPlan.tsx
@@ -3,11 +3,11 @@ import './CreateYourPlan.css'
 
 export const CreateYourPlan = () => {
   return (
-    <section className='create-your-plan rounded-xl  '>
-        <h1>Great coffee made simple.</h1>
-        <p>Start your mornings with the world’s best coffees. Try our expertly curated artisan 
+    <section className='create-your-plan rounded-xl  font-black flex flex-col items-center justify-center gap-6'>
+        <h1 className='w-64 text-center text-4xl'>Great coffee made simple.</h1>
+        <p className='w-64 font-normal text-center ' >Start your mornings with the world’s best coffees. Try our expertly curated artisan 
         coffees from our best roasters delivered directly to your door, at your schedule.</p>
-        <button>Create your plan</button>
+        <button className='bg-emerald-700 w-56 h-14 rounded-md'>Create your plan</button>
     </section>
   )
 }
