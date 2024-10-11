1e0a62bf1fcf3f366a08e767241dfb59fef1274c
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-10 21:00:00 -0400

    fdf

--- src/Components/Pages/Home/Ourcollection.tsx
+++ src/Components/Pages/Home/Ourcollection.tsx
@@ -1,48 +1,37 @@
 import "./Ourcollection.css";
+import gran from '../../../assets/home/desktop/image-gran-espresso.png'
+import planalto from '../../../assets/home/desktop/image-planalto.png'
+import piccollo from '../../../assets/home/desktop/image-piccollo.png'
+import danche from '../../../assets/home/desktop/image-danche.png'
 
 export const Ourcollection = () => {
   return (
-    <section className="our-collection">
-      <div className="our-collection-title">
-        <h1 className="  w-full text-center   text-4xl font-black ">
-          Our collection
-        </h1>
+    <section className="text-center">
+      <div className="">
+        <h2 className="text-black">our collection</h2>
       </div>
-
-      <div>
-        <img src={""} alt="" />
-        <h2 className="text-black">Gran Espresso</h2>
-        <p>
-          Light and flavorful blend with cocoa and black pepper for an intense
-          experience
-        </p>
+      <div className="flex flex-col gap-12" >
+        <div className="text-black flex flex-col gap-4 text-center">
+          <img src={gran} alt="our collection" />
+          <h4 className="text-2xl font-black">Gran Espresso</h4>
+          <p className="">Light and flavorful blend with cocoa and black pepper for an intense experience</p>
+        </div>
+        <div className="text-black flex flex-col gap-4 text-center">
+          <img src={planalto} alt="our collection" />
+          <h4 className="text-2xl font-black">Planalto</h4>
+          <p className="">Brazilian dark roast with rich and velvety body, and hints of fruits and nuts</p>
+        </div>
+        <div className="text-black flex flex-col gap-4 text-center">
+          <img src={piccollo} alt="our collection" />
+          <h4 className="text-2xl font-black">Piccollo</h4>
+          <p className="">Mild and smooth blend featuring notes of toasted almond and dried cherry </p>
+        </div>
+        <div className="text-black flex flex-col gap-4 text-center">
+          <img src={danche} alt="our collection" />
+          <h4 className="text-2xl font-black">Danche</h4>
+          <p className="">Ethiopian hand-harvested blend densely packed with vibrant fruit notes</p>
+        </div>
       </div>
-
-      <div>
-        <img src={""} alt="" />
-        <h2>Planalto</h2>
-        <p>
-          Brazilian dark roast with rich and velvety body, and hints of fruits
-          and nuts
-        </p>
-      </div>
-
-      <div>
-        <img src={""} alt="" />
-        <h2>Piccollo</h2>
-        <p>
-          Mild and smooth blend featuring notes of toasted almond and dried
-          cherry
-        </p>
-      </div>
-
-      <div>
-        <img src={""} alt="" />
-        <h2>Danche</h2>
-        <p>
-          Ethiopian hand-harvested blend densely packed with vibrant fruit notes
-        </p>
-      </div>
-    </section>
+   </section>
   );
 };
