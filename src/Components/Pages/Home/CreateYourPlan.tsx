7f4883925fbea261fde32f5560e4f7d7543e746b
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-13 19:53:02 -0400

    fdf

--- src/Components/Pages/Home/CreateYourPlan.tsx
+++ src/Components/Pages/Home/CreateYourPlan.tsx
@@ -8,7 +8,7 @@ export const CreateYourPlan = () => {
         <h1 className='w-64 text-center text-4xl md:text-5xl md:w-80 md:self-start lg:text-7xl lg:w-7/12 lg:text-left '>Great coffee made simple.</h1>
         <p className='w-64 font-normal opacity-80 text-center md:w-96 md:text-left  lg:self-start ' >Start your mornings with the world’s best coffees. Try our expertly curated artisan 
         coffees from our best roasters delivered directly to your door, at your schedule.</p>
-        <button className='bg-emerald-700 w-56 h-14 rounded-md md:self-start'>Create your plan</button>
+        <button className='bg-teal-600 w-56 h-14 rounded-md md:self-start'>Create your plan</button>
       </div>
     </section>
   )
--- src/Components/Pages/Home/HowitWorks.tsx
+++ src/Components/Pages/Home/HowitWorks.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
-import './Howitworks.css'
+import React from "react";
+import "./Howitworks.css";
 
 export const HowitWorks = () => {
   return (
-    <div className='text-black'>HowitWorks</div>
-  )
-}
+    <div className="howitworks text-black text-center flex flex-col gap-24  items-center">
+      <h3 className="text-2xl font-black text-gray-500 ">How it works</h3>
+
+      <div className="flex flex-col gap-14 ">
+        <div className="flex flex-col items-center gap-4">
+          <span className="text-7xl text-orange-200 font-black ">01</span>
+          <h4 className="text-3xl font-black">Pick your coffee</h4>
+          <p className="leading-7">
+            Select from our evolving range of artisan coffees. Our beans are
+            ethically sourced and we pay fair prices for them. There are new
+            coffees in all profiles every month for you to try out.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center gap-4">
+          <span className="text-7xl text-orange-200 font-black ">02</span>
+          <h4 className="text-3xl font-black">Pick your coffee</h4>
+          <p className="leading-7">
+            Select from our evolving range of artisan coffees. Our beans are
+            ethically sourced and we pay fair prices for them. There are new
+            coffees in all profiles every month for you to try out.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center gap-4">
+          <span className="text-7xl text-orange-200 font-black ">03</span>
+          <h4 className="text-3xl font-black">Pick your coffee</h4>
+          <p className="leading-7">
+            Select from our evolving range of artisan coffees. Our beans are
+            ethically sourced and we pay fair prices for them. There are new
+            coffees in all profiles every month for you to try out.
+          </p>
+        </div>
+      </div>
+      <button className="bg-teal-600 w-56 h-14 rounded-md md:self-start text-xl text-white font-black">Create your plan</button>
+    </div>
+  );
+};
