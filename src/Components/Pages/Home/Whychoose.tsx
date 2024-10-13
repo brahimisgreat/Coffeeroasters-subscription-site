0bae1de690cdd4506d4ed76405be45fab2d4129c
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-13 18:18:16 -0400

    fdf

--- src/Components/Pages/Home/Whychoose.tsx
+++ src/Components/Pages/Home/Whychoose.tsx
@@ -6,41 +6,48 @@ import truck from "../../../assets/home/desktop/icon-truck.svg";
 export const Whychoose = () => {
   return (
     <div className="relative flex flex-col items-center">
-      <div className="why flex flex-col items-center px-7 pt-16 gap-6">
+      <div className="why flex flex-col items-center px-7 pt-16 gap-6 md:px-1 md:h- ">
         <h3 className="text-3xl font-black">Why choose us?</h3>
-        <p className="opacity-80 text-center ">
+        <p className="opacity-80 text-center md:w-9/12">
           A large part of our role is choosing which particular coffees will be
           featured in our range. This means working closely with the best coffee
           growers to give you a more impactful experience on every level.
         </p>
       </div>
 
-      <div className="flex flex-col  w-72  absolute top-80 gap-6">
-        <div className="flex flex-col items-center justify-center gap-6 text-center px-10 rounded-lg h-96  text-white bg-teal-600">
-          <img src={bean} className="" />
-          <h4 className="text-2xl font-black mt-6">Best quality</h4>
-          <p className="opacity-80">
+      <div className="flex flex-col items-center w-full   absolute top-80 gap-6 md:top-1/2  ">
+        <div className="greensquare flex flex-col w-10/12 items-center justify-center gap-6 text-center px-10 rounded-lg h-96  text-white bg-teal-600 md:h-44 md:flex-row  ">
+          <img src={bean} className="ant" />
+          <div className="flex flex-col gap-4">
+
+          <h4 className="text-2xl font-black mt-6 md:text-left">Best quality</h4>
+          <p className="opacity-80 md:text-left">
             Discover an endless variety of the world’s best artisan coffee from
             each of our roasters.
           </p>
+          </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center gap-6 text-center px-10 rounded-lg h-96  text-white bg-teal-600">
+        <div className="greensquare flex flex-col  w-10/12 items-center justify-center gap-6 text-center px-10 rounded-lg h-96  text-white bg-teal-600 md:h-44 md:flex-row">
           <img src={gift} />
-          <h4 className="text-2xl font-black mt-6">Exclusive benefits</h4>
-          <p className="opacity-80">
+          <div>
+          <h4 className="text-2xl font-black mt-6 md:text-left">Exclusive benefits</h4>
+          <p className="opacity-80 md:text-left">
             Special offers and swag when you subscribe, including 30% off your
             first shipment.
           </p>
+          </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center gap-6 text-center px-10 rounded-lg h-96  text-white bg-teal-600">
-          <img src={truck} />
-          <h4 className="text-2xl font-black mt-6">Free shipping</h4>
-          <p className="opacity-80">
+        <div className="greensquare flex flex-col  w-10/12 items-center justify-center gap-6 text-center px-10 rounded-lg h-96  text-white bg-teal-600 md:h-44 md:flex-row ">
+          <img src={truck} className="" />
+          <div>
+          <h4 className="text-2xl font-black mt-6 md:text-left ">Free shipping</h4>
+          <p className="opacity-80 md:text-left">
             We cover the cost and coffee is delivered fast. Peak freshness:
             guaranteed.
           </p>
+          </div>
         </div>
       </div>
     </div>
