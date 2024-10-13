93b4ddf25809a7634fea147bcde9e6c0a7b01b8d
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-13 03:26:03 -0400

    fff

--- src/Components/Pages/Home/Ourcollection.tsx
+++ src/Components/Pages/Home/Ourcollection.tsx
@@ -15,7 +15,7 @@ export const Ourcollection = () => {
       <div className="flex flex-col gap-12 md:items-center lg:flex-row">
         <div className="text-black flex flex-col  text-center  md:items-center md:flex-row  lg:flex-col ">
           <img className="md:w-64" src={gran} alt="our collection" />
-          <div className="flex flex-col gap-4 md:text-left  ">
+          <div className="flex flex-col gap-4 md:text-left lg:text-center  ">
             <h4 className="text-2xl font-black">Gran Espresso</h4>
             <p className=" md:w-72">
               Light and flavorful blend with cocoa and black pepper for an
@@ -24,9 +24,9 @@ export const Ourcollection = () => {
           </div>
         </div>
 
-        <div className="text-black flex flex-col text-center md:items-center md:flex-row  ">
+        <div className="text-black flex flex-col text-center md:items-center md:flex-row lg:flex-col  ">
           <img className="md:w-64" src={planalto} alt="our collection" />
-          <div className="flex flex-col gap-4 md:text-left">
+          <div className="flex flex-col gap-4 md:text-left lg:text-center">
             <h4 className="text-2xl font-black">Planalto</h4>
             <p className="md:w-72">
               Brazilian dark roast with rich and velvety body, and hints of
@@ -35,9 +35,9 @@ export const Ourcollection = () => {
           </div>
         </div>
 
-        <div className="text-black flex flex-col  text-center md:flex-row ">
+        <div className="text-black flex flex-col  text-center md:flex-row  lg:flex-col">
           <img className="md:w-64" src={piccollo} alt="our collection" />
-          <div className="flex flex-col gap-4 md:text-left">
+          <div className="flex flex-col gap-4 md:text-left lg:text-center">
             <h4 className="text-2xl font-black">Piccollo</h4>
             <p className="md:w-72">
               Mild and smooth blend featuring notes of toasted almond and dried
@@ -46,9 +46,9 @@ export const Ourcollection = () => {
           </div>
         </div>
 
-        <div className="text-black flex flex-col  text-center md:flex-row ">
+        <div className="text-black flex flex-col  text-center md:flex-row lg:flex-col">
           <img className="md:w-64" src={danche} alt="our collection" />
-          <div className="flex flex-col gap-4 md:text-left">
+          <div className="flex flex-col gap-4 md:text-left lg:text-center">
             <h4 className="text-2xl font-black">Danche</h4>
             <p className="md:w-72">
               Ethiopian hand-harvested blend densely packed with vibrant fruit
