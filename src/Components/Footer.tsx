0af3a2a53781f4a1e127ea96ebf62dc3acc7afca
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-14 20:19:17 -0400

    fdfd

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./Footer.css";
-import logo from '../assets/shared/desktop/logo.svg'
+import logo from "../assets/shared/desktop/logo.svg";
+import facebook from "../assets/shared/desktop/icon-facebook.svg";
+import twitter from "../assets/shared/desktop/icon-twitter.svg";
+import instagram from "../assets/shared/desktop/icon-instagram.svg";
 
 export const Footer = () => {
+
+  
   return (
-    <footer>
-      <div className="footer">
+    <footer className="footer flex justify-center">
+      <div className="footerBaby flex flex-col items-center justify-center gap-10">
         <div className="footer__logo">
-          <img src={logo} alt="coffeeroasters logo" />
+          <img src={logo} className="bg-white w-52 h-6" alt="coffeeroasters logo" />
         </div>
 
         <div className="footer__menu">
-          <ul>
+          <ul className="flex flex-col items-center gap-4">
             <li>
               <a href="#">Home</a>
             </li>
@@ -24,18 +29,15 @@ export const Footer = () => {
           </ul>
         </div>
 
-        <div className="footer__social">
+        <div className="footer__social flex gap w-36  justify-between items-center">
           <a href="#">
-            <img src="/images/icon-facebook.svg" alt="facebook" />
+            <img src={facebook} alt="twitter" />
           </a>
           <a href="#">
-            <img src="/images/icon-twitter.svg" alt="twitter" />
+            <img src={twitter} alt="pinterest" />
           </a>
           <a href="#">
-            <img src="/images/icon-pinterest.svg" alt="pinterest" />
-          </a>
-          <a href="#">
-            <img src="/images/icon-instagram.svg" alt="instagram" />
+            <img src={instagram} alt="instagram" />
           </a>
         </div>
       </div>
--- src/Components/Pages/Home/HowitWorks.tsx
+++ src/Components/Pages/Home/HowitWorks.tsx
@@ -6,11 +6,19 @@ export const HowitWorks = () => {
     <div className="howitworks text-black text-center flex flex-col gap-24  items-center">
       <h3 className="text-2xl font-black text-gray-500 ">How it works</h3>
 
-      <div className="flex flex-col gap-14 ">
-        <div className="flex flex-col items-center gap-4">
-          <span className="text-7xl text-orange-200 font-black ">01</span>
-          <h4 className="text-3xl font-black">Pick your coffee</h4>
-          <p className="leading-7">
+      <div className="flex flex-row w-full  gap-56  border-black relative">
+        <div className="cir w-8 h-8 border-2 rounded-2xl border-teal-600  "/>
+        <div className="w-8 h-8 border-2 rounded-2xl border-teal-600 "/>
+        <div className="w-8 h-8 border-2 rounded-2xl border-teal-600 "/>
+        <div className="line w-3/4 border border-black absolute top-4 "/>
+
+      </div>
+
+      <div className="flex flex-col gap-14 md:flex-row border ">
+        <div className="flex flex-col items-center gap-4  ">
+          <span className="text-7xl text-orange-200 font-black md:text-left md:w-full ">01</span>
+          <h4 className="text-3xl font-black md:text-left">Pick your coffee</h4>
+          <p className="leading-7 md:text-left">
             Select from our evolving range of artisan coffees. Our beans are
             ethically sourced and we pay fair prices for them. There are new
             coffees in all profiles every month for you to try out.
@@ -18,9 +26,9 @@ export const HowitWorks = () => {
         </div>
 
         <div className="flex flex-col items-center gap-4">
-          <span className="text-7xl text-orange-200 font-black ">02</span>
-          <h4 className="text-3xl font-black">Pick your coffee</h4>
-          <p className="leading-7">
+          <span className="text-7xl text-orange-200 font-black md:text-left md:w-full ">02</span>
+          <h4 className="text-3xl font-black md:text-left">Pick your coffee</h4>
+          <p className="leading-7 md:text-left">
             Select from our evolving range of artisan coffees. Our beans are
             ethically sourced and we pay fair prices for them. There are new
             coffees in all profiles every month for you to try out.
@@ -28,9 +36,9 @@ export const HowitWorks = () => {
         </div>
 
         <div className="flex flex-col items-center gap-4">
-          <span className="text-7xl text-orange-200 font-black ">03</span>
-          <h4 className="text-3xl font-black">Pick your coffee</h4>
-          <p className="leading-7">
+          <span className="text-7xl text-orange-200 font-black md:text-left md:w-full ">03</span>
+          <h4 className="text-3xl font-black md:text-left">Pick your coffee</h4>
+          <p className="leading-7 md:text-left">
             Select from our evolving range of artisan coffees. Our beans are
             ethically sourced and we pay fair prices for them. There are new
             coffees in all profiles every month for you to try out.
