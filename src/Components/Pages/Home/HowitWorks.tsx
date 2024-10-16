b97c78b51564d4d490056c9a6e87518d935fc809
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-16 15:27:25 -0400

    ffd

--- src/Components/Pages/Home/HowitWorks.tsx
+++ src/Components/Pages/Home/HowitWorks.tsx
@@ -3,23 +3,23 @@ import "./Howitworks.css";
 
 export const HowitWorks = () => {
   return (
-    <div className="howitworks text-black text-center flex flex-col gap-24  items-cente ">
-      <h3 className="text-2xl font-black text-gray-500 md:text-left">How it works</h3>
+    <div className="howitworks text-black text-center flex flex-col gap-24 md:gap-16   items-center ">
+      <h3 className="text-2xl font-black text-gray-500 md:text-left md:w-full lg:ml-72">How it works</h3>
 
       
 
-      <div className="flex flex-col gap-14 md:flex-row  ">
-        <div className="flex flex-col items-center gap-4  ">
+      <div className="flex flex-col gap-14 md:flex-row  lg:items-center ">
+        <div className="flex flex-col items-center gap-4 lg:items-start lg:w-72 lg:gap-9">
           <span className="text-7xl text-orange-200 font-black md:text-left md:w-full ">01</span>
-          <h4 className="text-3xl font-black md:text-left">Pick your coffee</h4>
-          <p className="leading-7 md:text-left">
+          <h4 className="text-3xl font-black md:text-left  border  ">Pick your coffee</h4>
+          <p className="leading-7 md:text-left ">
             Select from our evolving range of artisan coffees. Our beans are
             ethically sourced and we pay fair prices for them. There are new
             coffees in all profiles every month for you to try out.
           </p>
         </div>
 
-        <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col items-center gap-4 lg:items-start lg:w-72 lg:gap-9">
           <span className="text-7xl text-orange-200 font-black md:text-left md:w-full ">02</span>
           <h4 className="text-3xl font-black md:text-left">Pick your coffee</h4>
           <p className="leading-7 md:text-left">
@@ -29,7 +29,7 @@ export const HowitWorks = () => {
           </p>
         </div>
 
-        <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col items-center gap-4 lg:items-start lg:w-72 lg:gap-9">
           <span className="text-7xl text-orange-200 font-black md:text-left md:w-full ">03</span>
           <h4 className="text-3xl font-black md:text-left">Pick your coffee</h4>
           <p className="leading-7 md:text-left">
@@ -39,7 +39,7 @@ export const HowitWorks = () => {
           </p>
         </div>
       </div>
-      <button className="bg-teal-600 w-56 h-14 rounded-md md:self-start text-xl text-white font-black">Create your plan</button>
+      <button className="bg-teal-600 w-56 h-14 rounded-md md:self-start text-xl text-white font-black lg:ml-36">Create your plan</button>
     </div>
   );
 };
--- src/Components/Pages/Home/Whychoose.tsx
+++ src/Components/Pages/Home/Whychoose.tsx
@@ -5,8 +5,8 @@ import truck from "../../../assets/home/desktop/icon-truck.svg";
 
 export const Whychoose = () => {
   return (
-    <div className="death relative h-4/6">
-    <div className="dad  relative flex flex-col items-center lg:h-1/2 ">
+    <div className="death h-4/6">
+    <div className="dad   flex flex-col items-center lg:h-1/2 ">
       <div className="why flex flex-col items-center px-7 pt-16 gap-6 md:px-1  ">
         <h3 className="text-3xl font-black ">Why choose us?</h3>
         <p className="opacity-80 text-center md:w-9/12 lg:w-5/12 leadin">
@@ -16,8 +16,8 @@ export const Whychoose = () => {
         </p>
       </div>
 
-      <div className="dothis absolute top-80 flex flex-col items-center w-full    gap-6 md:top-1/2 lg:flex-row lg:justify-center  ">
-        <div className="greensquare flex flex-col w-10/12 items-center justify-center gap-6 text-center px-10 rounded-lg h-96 text-white bg-teal-600 md:h-44 md:flex-row lg:flex-col lg:h-96 lg:max-w-80 ">
+      <div className="dothis absolute  flex flex-col items-center w-full    gap-6  lg:gap-10 lg:flex-row lg:justify-center  ">
+        <div className="greensquare flex flex-col w-8/12 items-center justify-center gap-6 text-center px-10 rounded-lg h-96 text-white bg-teal-600 md:h-44 md:flex-row lg:flex-col lg:h-96 lg:max-w-80 ">
           <img src={bean} className="ant" />
           <div className="flex flex-col gap-4 lg:items-center lg:justify-center">
 
@@ -29,7 +29,7 @@ export const Whychoose = () => {
           </div>
         </div>
 
-        <div className="greensquare flex flex-col  w-10/12 items-center justify-center gap-6 text-center px-10 rounded-lg h-96  text-white bg-teal-600 md:h-44 md:flex-row lg:flex-col lg:h-96 lg:max-w-80 ">
+        <div className="greensquare flex h-96  w-8/12 flex-col   items-center justify-center gap-6 text-center px-10 rounded-lg  text-white bg-teal-600 md:h-44 md:flex-row lg:flex-col lg:h-96 lg:max-w-80 ">
           <img src={gift} />
           <div className="flex flex-col gap-4 lg:items-center lg:justify-center ">
           <h4 className="text-2xl font-black mt-6 md:text-left lg:text-center">Exclusive benefits</h4>
@@ -40,7 +40,7 @@ export const Whychoose = () => {
           </div>
         </div>
 
-        <div className="greensquare flex flex-col  w-10/12 items-center justify-center gap-6 text-center px-10 rounded-lg h-96  text-white bg-teal-600 md:h-44 md:flex-row lg:flex-col lg:h-96 lg:items-center lg:max-w-80 ">
+        <div className="greensquare flex flex-col  w-8/12  items-center justify-center gap-6 text-center px-10 rounded-lg h-96  text-white bg-teal-600 md:h-44 md:flex-row lg:flex-col lg:h-96 lg:items-center lg:max-w-80 ">
           <img src={truck} className="" />
           <div className="flex flex-col gap-4 lg:items-center lg:justify-center" >
           <h4 className="text-2xl font-black mt-6 md:text-left lg:text-center ">Free shipping</h4>
