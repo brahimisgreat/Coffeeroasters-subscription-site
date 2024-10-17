b1b8f9874aeaba327ca2912d8deefb254ffdfdb8
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-17 19:27:05 -0400

    lets go

--- src/Components/Pages/About/About.tsx
+++ src/Components/Pages/About/About.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { AboutInfo } from './AboutInfo'
 import { OurCommitment } from './OurCommitment'
+import { Quality } from './Quality'
 
 export const About = () => {
   return (
     <main className='w-full flex flex-col gap-24'>
         <AboutInfo />
         <OurCommitment />
+        <Quality />
 
     </main>
   )
---
+++ src/Components/Pages/About/Quality.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Quality = () => {
+  return (
+    <section>
+      <div>
+        <h3>Uncompromising quality</h3>
+        <p>
+          Although we work with growers who pay close attention to all stages of
+          harvest and processing, we employ, on our end, a rigorous quality
+          control program to avoid over-roasting or baking the coffee dry. Every
+          bag of coffee is tagged with a roast date and batch number. Our goal
+          is to roast consistent, user-friendly coffee, so that brewing is easy
+          and enjoyable.
+        </p>
+      </div>
+    </section>
+  );
+};
