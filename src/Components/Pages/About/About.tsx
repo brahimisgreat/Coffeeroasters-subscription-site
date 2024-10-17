ed30cb6776e0290ab1f50bfd49bee14a5aae0f42
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-16 22:24:31 -0400

    ffd

--- src/Components/Pages/About/About.tsx
+++ src/Components/Pages/About/About.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { AboutInfo } from './AboutInfo'
+import { OurCommitment } from './OurCommitment'
 
 export const About = () => {
   return (
     <main className='w-full'>
         <AboutInfo />
+        <OurCommitment />
 
     </main>
   )
