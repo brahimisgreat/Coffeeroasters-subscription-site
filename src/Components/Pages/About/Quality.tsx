a54d995311e67bd01ff0f034465202a500e89378
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-17 19:42:14 -0400

    well done

--- src/Components/Pages/About/Quality.tsx
+++ src/Components/Pages/About/Quality.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import hero from '../../../assets/about/mobile/image-quality.jpg'
+import './Quality.css'
 
 export const Quality = () => {
   return (
-    <section>
-      <div>
-        <h3>Uncompromising quality</h3>
-        <p>
+    <section className="quality relative flex flex-col justify-end gap-2">
+      <div className="relative bottom-16">
+        <h3 className="text-3xl text-center font-bold mb-10 ">Uncompromising quality</h3>
+        <p className="text-center opacity-80">
           Although we work with growers who pay close attention to all stages of
           harvest and processing, we employ, on our end, a rigorous quality
           control program to avoid over-roasting or baking the coffee dry. Every
@@ -13,6 +15,10 @@ export const Quality = () => {
           is to roast consistent, user-friendly coffee, so that brewing is easy
           and enjoyable.
         </p>
+      </div>
+
+      <div className="absolute -top-14 right-9">
+        <img src={hero} className="w-64 rounded-lg" />
       </div>
     </section>
   );
