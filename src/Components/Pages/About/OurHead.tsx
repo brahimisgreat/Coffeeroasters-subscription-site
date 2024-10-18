5a2ee5bd3b957761b77d16f05c6498538d75399f
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-17 23:04:28 -0400

    what now?

--- src/Components/Pages/About/OurHead.tsx
+++ src/Components/Pages/About/OurHead.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './Ourhead.css'
+import uk from '../../../assets/about/desktop/illustration-uk.svg'
+import canada from '../../../assets/about/desktop/illustration-canada.svg'
+import australia from '../../../assets/about/desktop/illustration-australia.svg'
 
 export const Ourhead = () => {
   return (
-    <div>
-        
-    </div>
+    <section>
+        <div><h3>Our headquarters</h3></div>
+        <div>
+            <div>
+                <img src={uk} className=''/>
+                <h3></h3>
+                <div>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
