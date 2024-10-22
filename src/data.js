78d3429b27a43eae7ad794e43f9af6df7dc338ba
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-21 22:31:30 -0400

    vvccv

--- src/data.js
+++ src/data.js
@@ -14,18 +14,18 @@ const data = [
         question: "What type of coffee?",
         answers: [
             { id: 0, type: "Single Origin", desc:"Distinct, high quality coffee from a specific family-owned farm" },
-            { id: 1, type: "Filter", desc:"For pour over or drip methods like Aeropress, Chemex, and V60" },
-            { id: 2, type: "Espresso", desc:"Dense and finely ground beans for an intense, flavorful experience" },
+            { id: 1, type: "Decaf", desc:"Just like regular coffee, except the caffeine has been removed" },
+            { id: 2, type: "Blended", desc:"Combination of two or three dark roasted beans of organic coffees" },
         ]
 
     },
     {
-        id: 0,
-        question: "How do you drink your coffee?",
+        id: 2,
+        question: "How much would you like?",
         answers: [
-            { id: "a", type: "Capsule", desc:"Compatible with Nespresso systems and similar brewers" },
-            { id: "a", type: "Filter", desc:"For pour over or drip methods like Aeropress, Chemex, and V60" },
-            { id: "a", type: "Espresso", desc:"Dense and finely ground beans for an intense, flavorful experience" },
+            { id: 0, type: "250g", desc:"Perfect for the solo drinker. Yields about 12 delicious cups." },
+            { id: 1, type: "500g", desc:"Perfect option for a couple. Yields about 40 delectable cups." },
+            { id: 2, type: "1000g", desc:"Perfect for offices and events. Yields about 90 delightful cups." },
         ]
 
     },
