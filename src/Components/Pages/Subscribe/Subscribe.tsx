017dcf2d3c8842e4a30d97d0e80c0b611911c802
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-28 21:06:33 -0400

    getting close

--- src/Components/Pages/Subscribe/Subscribe.tsx
+++ src/Components/Pages/Subscribe/Subscribe.tsx
@@ -20,12 +20,12 @@ export const Subscribe = () => {
         <PickUrPlan />
         <DrinkCoffee  setSelected1={ setSelected1} selected1={selected1} setSelected2={ setSelected2} selected2={selected2} setSelected3={ setSelected3} selected3={selected3} setSelected4={ setSelected4} selected4={selected4} setSelected5={ setSelected5} selected5={selected5}/>
 
-        <div className=' '>
+        <div className='tinn flex flex-col items-center gap-10   '>
           <div className='last w-11/12 p-10 rounded-lg flex flex-col gap-3'>
             <h4 className='font-normal opacity-50'>Order Summary</h4>
             <p className='text-2xl leading-10'>“I drink my coffee as <span className='choice'>{selected1}</span>, with a<span className='choice'>{selected2}</span> type of bean. <span className='choice'>{selected3}</span> ground ala <span className='choice'>{selected4}</span>, sent to me <span className='choice'>{selected5}</span>.”</p>
           </div>
-          <button className='btn'>Create your plan</button>
+          <button className='btn text-xl'>Create your plan</button>
         </div>
     </div>
   )
