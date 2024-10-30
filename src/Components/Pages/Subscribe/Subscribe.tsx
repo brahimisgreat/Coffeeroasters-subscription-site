7f1f3b54a91183004a7c3a67a9a9d0d0dcdc8f0d
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-30 12:19:56 -0400

    getting close

--- src/Components/Pages/Subscribe/Subscribe.tsx
+++ src/Components/Pages/Subscribe/Subscribe.tsx
@@ -34,11 +34,14 @@ export const Subscribe = ({setCheckout, checkout}:Props) => {
         </div>
 
         <div className={checkout? 'os-container rounded-lg  flex flex-col gap-7   items-center' : "hidden"}>
-          <h3 className='os  text-white font-bold  rounded-lg  h-24 text-3xl flex items-center pl-10 w-full'>Order Summary</h3>
+          <div className='flex flex-col items-center w-72 gap-7'>
+          <h3 className='os  text-white font-bold   h-24 text-3xl flex items-center pl-10 '>Order Summary</h3>
           <p className='os-p1 text-2xl font-bold text-gray-500 md:leading-10 '>“I drink my coffee as <span className='choice'>{selected1}</span>, with a <span className='choice'>{selected2}</span> type of bean. <span className='choice'>{selected3}</span> ground ala <span className='choice'>{selected4}</span>, sent to me <span className='choice'>{selected5}</span>.”</p>
           <p className='os-p2 leading-6 opacity-80  text-black md:leading-7'>Is this correct? You can proceed to checkout or go back to plan selection if something is off. Subscription discount codes can also be redeemed at the checkout. </p>
           <button id="btn" className='btn text-white w-72 md:text-lg md:ml-44 'onClick={()=> setCheckout(false)} >Checkout <span className='spazz md:absolute  md:text-3xl md:font-bold md:text-black '>$14.00 / mo</span></button>
+          </div>
         </div>
+
     </div>
   )
 }
